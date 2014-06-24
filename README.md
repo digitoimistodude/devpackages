202837c555b3c068932f109db6a2a3aa7a21fa18
Roni Laukkarinen <roni@laukkarinen.info>
2014-06-25 01:31:38 +0300

    first commit

---
+++ README.md
@@ -0,0 +1,24 @@
+gulpfile-rolle
+=========================
+
+Just keeping track of changes in my gulpfile.js. This gulpfile is specifically meant for WordPress development on [wpstack-rolle](https://github.com/ronilaukkarinen/wpstack-rolle).
+
+##Usage
+
+Fill up package.json:
+
+        {
+          "name": "projectname",
+          "author": "Roni Laukkarinen"
+        }
+
+Add to your WordPress-theme functions.php:
+
+        // LiveReload/gulp
+        if ( $_SERVER['REMOTE_ADDR'] === '127.0.0.1' ){
+          add_action('wp_head', 'livereload');
+        }
+
+        function livereload() {
+        echo "<script>document.write('<script src=\"http://' + (location.host || 'localhost').split(':')[0] + ':35729/livereload.js?snipver=1\"></' + 'script>')</script>";
+        }
---
+++ gulpfile.js
@@ -0,0 +1,110 @@
+var gulp = require('gulp'),
+    compass = require('gulp-compass'),
+    sass = require('gulp-ruby-sass'),
+    autoprefixer = require('gulp-autoprefixer'),
+    minifycss = require('gulp-minify-css'),
+    jshint = require('gulp-jshint'),
+    uglify = require('gulp-uglify'),
+    imagemin = require('gulp-imagemin'),
+    rename = require('gulp-rename'),
+    clean = require('gulp-clean'),
+    concat = require('gulp-concat'),
+    notify = require('gulp-notify'),
+    cache = require('gulp-cache'),
+    header = require('gulp-header');
+    util = require('gulp-util');
+    livereload = require('gulp-livereload'),
+    currentDate = util.date(new Date(), 'dd-mm-yyyy');
+    pkg = require('./package.json');
+    banner = '/*! <%= pkg.name %> <%= currentDate %> - <%= pkg.author %> */\n';
+
+// Compass without config.rb:
+
+gulp.task('compass', function() {
+  gulp.src('content/themes/themename/sass/layout.scss')
+  .pipe(compass({
+    css: 'content/themes/themename/css',
+    sass: 'content/themes/themename/sass',
+    image: 'content/themes/themename/images'
+     // ,require: ['sassline']
+      }))
+  .on('error', function(err) {
+    // Would like to catch the error here
+      })
+  .pipe(minifycss({keepBreaks:false,keepSpecialComments:0,}))
+  .pipe(gulp.dest('content/themes/themename/css'))
+  .pipe(livereload())
+  .pipe(notify({ message: 'Compass complete' }));
+    });
+
+// If not using compass:
+
+// gulp.task('styles', function() {
+//   return gulp.src('content/themes/jentafon/sass/layout.scss')
+//     .pipe(sass({ style: 'expanded', }))
+//     .pipe(autoprefixer('last 2 version', 'safari 5', 'ie 8', 'ie 9', 'opera 12.1', 'ios 6', 'android 4'))
+//     .pipe(gulp.dest('content/themes/jentafon/css'))
+//     .pipe(rename({ suffix: '.min' }))
+//     .pipe(minifycss())
+//     .pipe(gulp.dest('content/themes/jentafon/css'))
+//     .pipe(notify({ message: 'Styles task complete' }));
+    // });
+
+gulp.task('scripts', function() {
+  //gulp.src('content/themes/themename/js/*.js')
+  gulp.src(
+    [
+    'content/themes/themename/js/jquery-1.7.1.min.js',
+    'content/themes/themename/inc/fancybox/source/jquery.fancybox.pack.js',
+    'content/themes/themename/js/highlight.min.js',
+    'content/themes/themename/js/jquery.simpleLastFM.min.js',
+    'content/themes/themename/inc/twitter/jquery.tweet.min.js',
+    'content/themes/themename/js/bootstrap.min.js',
+    'content/themes/themename/js/pongstagr.am.min.js',
+    'content/themes/themename/js/placeholders.min.js',
+    'content/themes/themename/js/skip-link-focus-fix.js',
+    'content/themes/themename/js/jquery.smooth-scroll.min.js',
+    'content/plugins/bj-lazy-load/js/combined.min.js',
+    'content/themes/themename/js/scripts.min.js' 
+    ])
+    //.pipe(jshint('.jshintrc'))
+    //.pipe(jshint.reporter('default'))
+    .pipe(concat('all.js'))
+    .pipe(uglify({preserveComments: false, compress: true, mangle: true}))
+    .pipe(header(banner, {pkg: pkg, currentDate: currentDate}))
+    .pipe(gulp.dest('content/themes/themename/js/'))
+    .pipe(livereload())
+    .pipe(notify({ message: 'scripts task complete' }));
+});
+
+gulp.task('images', function() {
+  return gulp.src('content/themes/themename/images/*')
+    .pipe(cache(imagemin({ optimizationLevel: 5, progressive: true, interlaced: true })))
+    .pipe(livereload())
+    .pipe(gulp.dest('content/themes/themename/images/optimized'));
+    });
+
+gulp.task('php', function(){  
+    gulp.src('*.php')
+    .pipe(livereload())
+    .pipe(notify({ message: 'php-file was reloaded' }));
+})
+
+gulp.task('server', function(){  
+    gulp.src('*.php')
+    .pipe(livereload())
+    .pipe(notify({ message: 'php-file was reloaded' }));
+})
+
+gulp.task('watch', function() {
+
+  livereload.listen();
+
+  gulp.watch('content/themes/themename/*.php', ['php']);
+  gulp.watch('content/themes/themename/sass/*.scss', ['compass']);
+  gulp.watch('content/themes/themename/js/scripts.js', ['scripts']);
+  gulp.watch('content/themes/themename/images/*', ['images']);
+
+});
+
+gulp.task('default', function() { gulp.start('compass', 'scripts', 'images'); });  
