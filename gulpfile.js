51900d4623ba6b9deb426452ac0ba8567a20f78c
Roni Laukkarinen <roni@laukkarinen.info>
2017-05-29 11:10:28 +0300

    Add error handling for gulp-uglify

--- gulpfile.js
+++ gulpfile.js
@@ -161,7 +161,12 @@ gulp.task('js', function() {
           themeDir + '/js/src/scripts.js'
         ])
         .pipe(concat('all.js'))
-        .pipe(uglify({preserveComments: false, compress: true, mangle: true}).on('error',function(e){console.log('\x07',e.message);return this.end();}))
+        .pipe(uglify({
+          compress: true,
+          mangle: true}).on('error', function(err) {
+            util.log(util.colors.red('[Error]'), err.toString());
+            this.emit('end');
+          }))
         .pipe(header(banner, {pkg: pkg, currentDate: currentDate}))
         .pipe(gulp.dest(jsDest));
 });
