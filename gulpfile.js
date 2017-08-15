3ca8eb782f501472d8b97a54684eaad6b8516f50
Roni Laukkarinen <roni@laukkarinen.info>
2017-08-15 09:50:56 +0300

    Roll back to Chrome

--- gulpfile.js
+++ gulpfile.js
@@ -73,7 +73,7 @@ gulp.task('browsersync', function() {
 
   browserSync.init(files, {
     proxy: "PROJECTNAME.test",
-    browser: "FirefoxDeveloperEdition",
+    browser: "Google Chrome",
     notify: true
   });
 
