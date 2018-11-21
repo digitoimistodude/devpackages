a942f940646ea7ec549d5a706e81c29443111c18
Roni Laukkarinen <roni@laukkarinen.info>
2018-11-21 13:28:08 +0200

    Add a11y task

--- gulpfile.js
+++ gulpfile.js
@@ -364,7 +364,7 @@ gulp.task('js-watch', ['js'], browsersync.reload);
 gulp.task('watch', ['browsersync'], function() {
 
   gulp.watch(sassSrc, ['styles', 'scss-lint']).on( 'change', helpers );
-  gulp.watch(phpSrc, ['phpcs', 'validatehtml']);
+  gulp.watch(phpSrc, ['phpcs', 'validatehtml', 'a11y']);
   gulp.watch(jsSrc, ['js-watch']);
 
 });
