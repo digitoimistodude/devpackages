ea3a306d5342762d892532df086044f00a628a46
Roni Laukkarinen <roni@laukkarinen.info>
2019-02-01 10:50:29 +0200

    Replace gulp-autoprefixer with postcss and latest autoprefixer plugin

--- gulpfile.js
+++ gulpfile.js
@@ -125,6 +125,13 @@ gulp.task('scss-lint', function() {
 
 gulp.task('styles', function() {
 
+    // Adds browser fallback (eg. -webkit, -moz, etc.)
+    // When a browser dies, Autoprefixer will automatically stop writing prefixes for that browser
+    // Source: https://css-tricks.com/css-grid-in-ie-css-grid-and-the-new-autoprefixer/
+    var plugins = [
+        autoprefixer({grid: true, browsers: ['>1%']})
+    ];
+
     // Save compressed version
     gulp.src(sassFile)
 
@@ -144,7 +151,7 @@ gulp.task('styles', function() {
     }))
 
     .on('error', handleError('styles'))
-    .pipe(prefix('last 3 version', 'safari 5', 'ie 9', 'opera 12.1', 'ios 6', 'android 4')) // Adds browser prefixes (eg. -webkit, -moz, etc.)
+    .pipe(postcss(plugins))
     .pipe(pixrem())
     .pipe(cleancss({
       compatibility: 'ie11',
@@ -186,7 +193,7 @@ gulp.task('styles', function() {
     }))
 
     .on('error', handleError('styles'))
-    .pipe(prefix('last 3 version', 'safari 5', 'ie 9', 'opera 12.1', 'ios 6', 'android 4')) // Adds browser prefixes (eg. -webkit, -moz, etc.)
+    .pipe(postcss(plugins))
     .pipe(pixrem())
 
     // Process the expanded output with Stylefmt
