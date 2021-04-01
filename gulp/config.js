1a39a0f9b7c0b65e592f0a28d75cbe0456929c17
Roni Laukkarinen <roni@dude.fi>
2021-04-01 14:00:25 +0300

    Remove unused watch

--- gulp/config.js
+++ gulp/config.js
@@ -49,10 +49,6 @@ module.exports = {
         cert: "/var/www/certs/localhost.pem",
       }
     },
-    watch: [
-      themeDir + '**/*.php',
-      themeDir + 'js/src/**/*.js'
-    ]
   },
   styles: {
     gutenberg: themeDir + 'sass/base/gutenberg.scss',
